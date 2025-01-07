7bc6d02694b84ef99195a65e86d6664712cb8f58
rajeshchintalapudi <rajeshchintalapudi0@gmail.com>
2025-01-07 11:28:04 +0530

    triangle area

--- demo13.java
+++ demo13.java
@@ -1,22 +1,17 @@
-public class demo13 {package Core_Java;
+public class demo13 {public class Demo013New {
 
-    class TriangleAreaDemo1 {
-    
-        double getTriangleArea(int s1,int s2,int s3){
-            double s = (s1 + s2 + s3) / 2;
-            double ta = Math.sqrt(s * (s - s1) * (s - s2) * (s-s3));
-            return ta;
-        }
-        
-    }
-    public class Demo013 {
-        public static void main(String[] args) {
-            int s1 = 5, s2 = 6, s3 =7;		
-            TriangleAreaDemo1 ta = new TriangleAreaDemo1();
-            ta.getTriangleArea(s1, s2, s3);
-            
-            System.out.println("The triangle area is : " + ta);
-        }
-    }
+	public static double getTriangleArea(int s1,int s2,int s3){
+		double s = (s1 + s2 + s3) / 2;
+		double ta = Math.sqrt(s * (s - s1) * (s - s2) * (s-s3));
+		return ta;
+	}
+	public static void main(String[] args) {
+		int s1 = 5, s2 = 6, s3 =7;
+		
+		double ta = getTriangleArea(s1,s2,s3);
+		
+		System.out.println("The triangle area is : " + ta);
+	}
+}
 
 }
