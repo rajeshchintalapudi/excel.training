833705a1dc34dabfc81ac5994e0f1dec09c1d552
rajeshchintalapudi <rajeshchintalapudi0@gmail.com>
2025-01-07 11:08:57 +0530

    do while

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main(String args[]){
+        String Name = "Kishore";
+        String pwd = "kishore@123";
+        while ((Name == "Kishore") && (pwd == "Kishore@123")){
+            System.out.println("Hello Kishore welcome to mallareddy");
+            break;
+        }
+    }   
+}
