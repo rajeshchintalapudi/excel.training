a86192bd2be75f4936876b411663f5bc125c7fef
rajeshchintalapudi <rajeshchintalapudi0@gmail.com>
2025-02-12 12:07:13 +0530

    Even element places in an array

---
+++ Demo067.java
@@ -0,0 +1,18 @@
+package Core_Java;
+
+public class Demo067 {
+ 
+    public static void main(String[] args) {
+        int [] arr ={12,20,30,45,11};
+        int n = arr.length;
+        int count = 0;
+        System.out.println("Even elements are: ");
+        for (int i = 1; i < n; i++) {
+            if(i % 2 ==0){
+                System.out.println("arr["+(i)+"] : "+arr[i]);
+            }
+           
+        }
+        
+    }
+}
